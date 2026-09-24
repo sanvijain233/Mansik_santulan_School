0623d7543fa7f7735f641acd08d2c4e740c5b618
Sanvi Jain <sanvijain233@gmail.com>
2026-09-24 10:44:55 +0530

    New js file with deployed backened link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-school.onrender.com";
+  const API_BASE = "https://mansik-santulan-backend.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
