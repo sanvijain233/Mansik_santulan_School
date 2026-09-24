7d185a0dd232fc6af6ed7ca200e2b19b0bf13725
Sanvi Jain <sanvijain233@gmail.com>
2026-09-24 11:00:26 +0530

    Connect frontend with deployed backend

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://mansik-santulan-school-1.onrender.com",
+        "https://mansik-santulan-school-frontened.onrender.com",
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
